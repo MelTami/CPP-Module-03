bd5aa8cd4811d2089e2adf9700801d4db6e134df
Melissa Vavassori <92276636+MelTami@users.noreply.github.com>
2024-03-14 17:49:03 +0000

    ex00 done

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: mvavasso <mvavasso@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 20:40:58 by mvavasso          #+#    #+#             */
-/*   Updated: 2024/03/10 23:47:36 by mvavasso         ###   ########.fr       */
+/*   Updated: 2024/03/14 17:35:07 by mvavasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,41 @@
 
 #include <iostream>
 #include <string>
+#include "colors.hpp"
 
 class ClapTrap
 {
 private:
     std::string     name;
-    int             hitpoints;
-    int             energypoints;
-    int             attackdamage;
+    std::string     color;
+    unsigned int    hitpoints;
+    unsigned int    energypoints;
+    unsigned int    attackdamage;
 public:
     ClapTrap();
-    ClapTrap( std::string name );
-	ClapTrap( ClapTrap const &rhs );
+    ClapTrap( const std::string name );
+    ClapTrap(const std::string &name, const std::string &color, unsigned int const hp, unsigned int const ep, unsigned int const ad);
+	ClapTrap( ClapTrap const &copy );
     ~ClapTrap();
 
-    ClapTrap		&operator=( ClapTrap const &rhs );
+    ClapTrap		&operator=( ClapTrap const &copy );
     void            attack(const std::string& target);
     void            takeDamage(unsigned int amount);
     void            beRepaired(unsigned int amount);
 
+    int getHitPoints(void) const;
+	int getenergypoints(void) const;
+	int getattackdamage(void) const;
+	void sethitpoints(unsigned int const hp);
+	void setenergypoints(unsigned int const ep);
+	void setattackdamage(unsigned int const ad);
 
+	std::string getName(void) const;
+	void setName(const std::string &name);
+
+    bool isDead(void);
+
+	std::ostream&operator<<(std::ostream& os);
 };
 
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,49 @@
 /*   By: mvavasso <mvavasso@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 20:41:02 by mvavasso          #+#    #+#             */
-/*   Updated: 2024/03/10 20:41:03 by mvavasso         ###   ########.fr       */
+/*   Updated: 2024/03/14 17:39:13 by mvavasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int main (void)
+{
+    //name, color name, hp, energy, attack damage
+	std::cout << COLOR_BHWHITE << "CONSTRUCTORS" << COLOR_RESET << std::endl;
+	ClapTrap Foo("Foo", COLOR_BHYELLOW, 30, 2, 8);
+	ClapTrap Boo("Boo", COLOR_BHRED, 26, 6, 10);
+	ClapTrap Goo("Goo", COLOR_BHCYAN, 20, 8, 7);
+	ClapTrap Doo("Doo", COLOR_BHMAGENTA, 18, 4, 6);
+	ClapTrap Loo("Loo", COLOR_BHGREEN, 14, 10, 4);
+
+	std::cout << std::endl << COLOR_BHWHITE << "THE FIGHT" << COLOR_RESET << std::endl;
+	//KILL
+	Doo.attack(Loo.getName());
+	Loo.takeDamage(6);
+	Foo.attack(Loo.getName());
+	Loo.takeDamage(8);
+
+	//NO ENERGY POINTS
+	Foo.attack(Doo.getName());
+    Doo.takeDamage(8);
+	Foo.beRepaired(6);
+
+	//ATTACK
+	Boo.attack(Goo.getName());
+
+	//TAKE NO DAMAGE
+	Goo.takeDamage(0);
+
+	//PLAYER IS DEAD (no side effects)
+	Loo.attack(Foo.getName());
+	Loo.takeDamage(4);
+	Loo.beRepaired(6);
+
+	//HEAL
+	Doo.beRepaired(4);
+
+	std::cout << std::endl << COLOR_BHWHITE << "DESTRUCTORS" << COLOR_RESET;
+
+	return 0;
+}
